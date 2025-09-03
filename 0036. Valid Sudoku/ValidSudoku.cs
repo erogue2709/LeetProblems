3cb281cb8af4f97ac9735aef339cdd3aa6a635a2
erogue2709 <decourrieremax@hotmail.fr>
2025-09-03 20:51:04 +0200

    26-80-595 + refacto

--- 0036. Valid Sudoku/ValidSudoku.cs
+++ 0036. Valid Sudoku/ValidSudoku.cs
@@ -1,39 +1,21 @@
 public class Solution {
 
-    private static bool CheckPossibilities(char[][] board, int rowValue, int columnValue){
+    private static bool CheckValide(char[][] board, int rowValue, int columnValue){
         for(byte cursor = 0; cursor < 9; cursor++)
         {
-            if(cursor != rowValue && board[cursor][columnValue] == board[rowValue][columnValue])
-                return false;
-            if(cursor != columnValue && board[rowValue][cursor] == board[rowValue][columnValue])
+            if(cursor != rowValue && board[cursor][columnValue] == board[rowValue][columnValue]
+                || cursor != columnValue && board[rowValue][cursor] == board[rowValue][columnValue])
                 return false;
         }
-        int[] smallRows;
-        int[] smallColumns;
-        if(rowValue < 3)
-            smallRows = new int[3] {0,1,2};
-        else if (rowValue < 6)
-            smallRows = new int[3] {3,4,5};
-        else
-            smallRows = new int[3] {6,7,8};
-        if(columnValue < 3)
-            smallColumns  = new int[3] {0,1,2};
-        else if (columnValue < 6)
-            smallColumns  = new int[3] {3,4,5};
-        else
-            smallColumns  = new int[3] {6,7,8};
+
+        int[] smallRows = rowValue < 3? [0,1,2] : rowValue < 6 ? [3,4,5] : [6,7,8];
+        int[] smallColumns = columnValue < 3? [0,1,2] : columnValue < 6 ? [3,4,5] : [6,7,8];
+
         foreach(int smallRow in smallRows)
-        {
-            if(smallRow == rowValue)
-                continue;
             foreach(int smallColumn in smallColumns)
-            {
-                if(smallColumn == columnValue)
-                    continue;
-                if(board[smallRow][smallColumn] == board[rowValue][columnValue])
+                if(smallRow != rowValue && smallColumn != columnValue && board[smallRow][smallColumn] == board[rowValue][columnValue])
                     return false;
-            }
-        }
+
         return true;
     }
 
@@ -44,7 +26,7 @@ public class Solution {
             {
                 if(board[row][column] != '.')
                 {
-                    if(!CheckPossibilities(board, row, column))
+                    if(!CheckValide(board, row, column))
                         return false;
                 }
             }
