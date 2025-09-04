e68a5a70b08e967d013489df98d16ed67fa77e37
erogue2709 <decourrieremax@hotmail.fr>
2025-09-04 11:59:45 +0200

    more refacto; still half good

--- 0054. Spiral Matrix/SpiralMatrix.cs
+++ 0054. Spiral Matrix/SpiralMatrix.cs
@@ -14,52 +14,83 @@ public class Solution {
         return _k == _nbValues;
     }
 
-    public IList<int> SpiralOrder(int[][] matrix) {
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
         int x = 0;
         int y = 0;
-        int xmax = matrix[0].Length-1;
-        int ymax = matrix.Length-1;
+        int xmax = matrix[0].Length - 1;
+        int ymax = matrix.Length - 1;
 
         _nbValues = matrix.Length * matrix[0].Length;
         _k = 0;
         int[] result = new int[_nbValues];
-        while(true){
-            if(extractValues(matrix, result, x, xmax, y++, true, true))
+        while (true)
+        {
+            if (extractValues(matrix, result, x, xmax, y++, true, true))
                 break;
-            if(extractValues(matrix, result, y, ymax, xmax--, true, false))
+            if (extractValues(matrix, result, y, ymax, xmax--, true, false))
                 break;
-            if(extractValues(matrix, result, xmax, x, ymax--, false, true))
+            if (extractValues(matrix, result, xmax, x, ymax--, false, true))
                 break;
-            if(extractValues(matrix, result, ymax, y, x++, false, false))
+            if (extractValues(matrix, result, ymax, y, x++, false, false))
                 break;
         }
         return result;
     }
 }
 
-// refacto
-public class Solution {
+// half refacto
+public class Solution
+{
     private int _k, _nbValues;
     private int[] _result;
 
-    private void Parkour(int[][] matrix, int val, int valmax, int constVal, bool increase, bool isRow){
-        for(int idx = val; increase ? idx <= valmax : idx >= valmax; idx += increase ? 1 : -1)
+    private void Parkour(int[][] matrix, int val, int valmax, int constVal, bool increase, bool isRow)
+    {
+        for (int idx = val; increase ? idx <= valmax : idx >= valmax; idx += increase ? 1 : -1)
             _result[_k++] = !isRow ? matrix[idx][constVal] : matrix[constVal][idx];
     }
 
-    public IList<int> SpiralOrder(int[][] matrix) {
-        int left = 0, top = 0, right = matrix[0].Length-1, bottom = matrix.Length-1;
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        int left = 0, top = 0, right = matrix[0].Length - 1, bottom = matrix.Length - 1;
 
         _nbValues = matrix.Length * matrix[0].Length;
         _result = new int[_nbValues];
         _k = 0;
-        while(true){
+        while (true)
+        {
             Parkour(matrix, left, right, top++, true, true);
             Parkour(matrix, top, bottom, right--, true, false);
             if (!(left < right && top < bottom)) break;
             Parkour(matrix, right, left, bottom--, false, true);
             Parkour(matrix, bottom, top, left++, false, false);
-            if(left < right && top < bottom) break;
+            if (left < right && top < bottom) break;
+        }
+        return _result;
+    }
+}
+
+
+// best munis notyet working :/
+public class Solution {
+    public IList<int> SpiralOrder(int[][] matrix) {
+        int left = 0, top = 0, right = matrix[0].Length-1, bottom = matrix.Length-1;
+        int[]  _result = new int[matrix.Length * matrix[0].Length];
+        int _k = 0;
+        while(_k != matrix.Length * matrix[0].Length){
+            for(int idx = left; idx <= right; idx++)
+                _result[_k++] = matrix[top][idx];
+            top++;
+            for(int idx = top; idx <= bottom; idx++)
+                _result[_k++] = matrix[idx][right];
+            right--;
+            for(int idx = right; idx >= left; idx--)
+                _result[_k++] = matrix[bottom][idx];
+            bottom--;
+            for(int idx = bottom; idx >= top; idx--)
+                _result[_k++] = matrix[idx][left];
+            left++;
         }
         return _result;
     }
